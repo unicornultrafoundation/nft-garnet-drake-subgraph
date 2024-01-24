b0b3b9a8bfc45a0d21a4d702a352e4a4d8022433
ngdlong91 <ngdlong91@gmail.com>
2024-01-24 11:09:13 +0700

    Update subgraph

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
 import {Address} from "@graphprotocol/graph-ts";
 
 
-export const ContractAddress = Address.fromString("0xC102BfEeE0674798Df49618E12b45FcD8e7C0EC0")
+export const ContractAddress = Address.fromString("0x3F797f942Db1469bA238EBA67857a2da02EF058D")
